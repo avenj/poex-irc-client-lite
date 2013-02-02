d246fc7585b5a30d94d9f328ab64a1ff141cc30f
Jon Portnoy <avenj@cobaltirc.org>
2013-02-02 18:35:27 -0500

    eg/ cleanup
    	modified:   eg/client_lite_bot.pl

--- eg/client_lite_bot.pl
+++ eg/client_lite_bot.pl
@@ -14,9 +14,9 @@ POE::Session->create(
   package_states => [
     main => [ qw/
       _start
-      E_irc_public_msg
-      E_irc_ctcp_version
-      E_irc_001
+      cli_irc_public_msg
+      cli_irc_ctcp_version
+      cli_irc_001
     / ],
   ],
 );
@@ -25,22 +25,22 @@ $poe_kernel->run;
 sub _start {
   my ($kern, $heap) = @_[KERNEL, HEAP];
   $heap->{irc} = POEx::IRC::Client::Lite->new(
-    event_prefix => 'E_',
+    event_prefix => 'cli_',
     server   => $server,
     nick     => $nickname,
     username => $username,
   )->connect()
 }
 
-sub E_irc_001 {
+sub cli_irc_001 {
   my ($kern, $heap, $ev) = @_[KERNEL, HEAP, ARG0];
 
   ## Chainable methods.
   my $irc = $heap->{irc};
-  $irc->join(@channels)->privmsg(join(',', @channels), "hello!");
+  $irc->join(@channels)->privmsg(join(',', @channels), "hello there!");
 }
 
-sub E_irc_public_msg {
+sub cli_irc_public_msg {
   my ($kern, $heap, $ev) = @_[KERNEL, HEAP, ARG0];
   my ($target, $string)  = @{ $ev->params };
 
@@ -49,7 +49,7 @@ sub E_irc_public_msg {
   }
 }
 
-sub E_irc_ctcp_version {
+sub cli_irc_ctcp_version {
   my ($kern, $heap, $ev) = @_[KERNEL, HEAP, ARG0];
 
   my $from = parse_user( $ev->prefix );
